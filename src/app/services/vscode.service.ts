1f1e11af275ebc401dc5948eca0323a9834802fd
Sebastian Rath <git@sebastianrath.io>
2023-12-24 18:35:04 -0500

    Remove unused code

--- src/app/services/vscode.service.ts
+++ src/app/services/vscode.service.ts
@@ -33,34 +33,6 @@ export class VsCodeService {
         if (typeof acquireVsCodeApi !== 'undefined') {
             this._vsCode = acquireVsCodeApi();
         }
-
-        window.addEventListener('message', (event: Event) => {
-            const { type, data, requestId } = (event as MessageEvent).data;
-            switch (type) {
-                // Incoming response for request via 'postMessageWithResponse'
-                case 'callbackResponse': {
-                    const callback = this._callbacks.get(requestId);
-                    if (callback) {
-                        callback(data);
-                    }
-                    break;
-                }
-                default: {
-                    this.message$.next(event as unknown as VsCodeMessage);
-                    break;
-                }
-            }
-        });
-    }
-
-    postMessageWithResponse<R = unknown>(type: string, data: R): Promise<unknown> {
-        if (!this._vsCode) {
-            throw new Error('vscode not found');
-        }
-        const requestId = this._requestId++;
-        const p = new Promise<unknown>(resolve => this._callbacks.set(requestId, resolve));
-        void this._vsCode.postMessage({ type, requestId, data });
-        return p;
     }
 
     postMessage(opts: { type: string, requestId: number, data: unknown }): void {
